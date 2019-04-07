d7807418aeab0780ddcc7ec08d90602ff18bc5ae
Tanja Küry <tanja.kuery@gmail.com>
2019-04-07 23:12:02 +0200

    Updated bash script for cnn

--- Task_2_b_c_d/optimizeCnn.sh
+++ Task_2_b_c_d/optimizeCnn.sh
@@ -6,21 +6,13 @@ source activate deepdiva
 if [ -d datasets/mnist ]
 then
     echo dataset found!
+    export PYTHONPATH=.
+    for i in 5 10 20
+    do
+    python template/RunMe.py --ignoregit --lr "0.001" --epochs "$i" --dataset-folder datasets/mnist --no-cuda --model-name PR_CNN --experiment-name cnn_bash --output-folder output/cnn_opt
+    python template/RunMe.py --ignoregit --lr "0.05" --epochs "$i" --dataset-folder datasets/mnist --no-cuda --model-name PR_CNN --experiment-name cnn_bash --output-folder output/cnn_opt
+    python template/RunMe.py --ignoregit --lr "0.1" --epochs "$i" --dataset-folder datasets/mnist --no-cuda --model-name PR_CNN --experiment-name cnn_bash --output-folder output/cnn_opt
+    done
 else
     echo no dataset found in folder 'datasets/mnist'
 fi
-
-export PYTHONPATH=.
-
-for i in 15 25
-    do
-    python template/RunMe.py --ignoregit --lr "0.001" --epochs "$i" --dataset-folder datasets/mnist --no-cuda --model-name PR_CNN --experiment-name cnn_bash --output-folder output/cnn_runs
-    python template/RunMe.py --ignoregit --lr "0.005" --epochs "$i" --dataset-folder datasets/mnist --no-cuda --model-name PR_CNN --experiment-name cnn_bash --output-folder output/cnn_runs
-    for ((j=1; j<10; j+=1))
-        do
-        echo "epochs "$i" lr 0.0$j"
-        python template/RunMe.py --ignoregit --lr "0.0$j" --epochs "$i" --dataset-folder datasets/mnist --no-cuda --model-name PR_CNN --experiment-name cnn_bash --output-folder output/cnn_runs
-        done
-        echo "epochs "$i" lr 0.1"
-        python template/RunMe.py --ignoregit --lr "0.1" --epochs "$i" --dataset-folder datasets/mnist --no-cuda --model-name PR_CNN --experiment-name cnn_bash --output-folder output/cnn_runs
-    done
