2f2eddb2a0aebcdedde1e143b3db96d926d9cfbb
Tanja Küry <tanja.kuery@gmail.com>
2019-04-08 22:53:57 +0200

    Finished Task 2d

--- Task_2_b_c_d/optimizeMlp.sh
+++ Task_2_b_c_d/optimizeMlp.sh
@@ -12,18 +12,12 @@ fi
 
 export PYTHONPATH=.
 
-for i in 10 20 30
+for i in 10 20 5
     do
     for h in 10 50 100
         do
-        echo "epochs "$i" hidden_nodes "$h" lr 0.001"
-        python template/RunMe.py --ignoregit --lr 0.001 --epochs "$i" --dataset-folder datasets/mnist --no-cuda --model-name PR_CNN --experiment-name cnn_bash --output-folder output/mlp
-        echo "epochs "$i" hidden_nodes "$h" lr 0.005 "
-        python template/RunMe.py --ignoregit --lr 0.005 --epochs "$i" --hidden_nodes "$h" --dataset-folder datasets/mnist --no-cuda --model-name PR_CNN --experiment-name cnn_bash --output-folder output/mlp
-        for ((j=1; j<10; j+=1))
-            do
-            echo "epochs "$i" hidden_nodes "$h" lr "0.0$j""
-            python template/RunMe.py --ignoregit --lr "0.0$j" --epochs "$i" --dataset-folder datasets/mnist --no-cuda --model-name PR_CNN --experiment-name cnn_bash --output-folder output/mlp
-            done
+        python template/RunMe.py --ignoregit --lr 0.001 --epochs "$i" --hidden-nodes "$h" --dataset-folder datasets/mnist --no-cuda --model-name MLP --experiment-name mlp --output-folder output/mlp
+        python template/RunMe.py --ignoregit --lr 0.05 --epochs "$i" --hidden-nodes "$h" --dataset-folder datasets/mnist --no-cuda --model-name MLP --experiment-name mlp --output-folder output/mlp
+        python template/RunMe.py --ignoregit --lr 0.1 --epochs "$i" --hidden-nodes "$h" --dataset-folder datasets/mnist --no-cuda --model-name MLP --experiment-name mlp --output-folder output/mlp
         done
     done
